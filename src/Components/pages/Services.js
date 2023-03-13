2eedeb7726176dafdd78b767cb524e4d4b8a780b
Tanishq dublish <100364028+Tanishq2003be@users.noreply.github.com>
2023-03-14 00:47:48 +0530

    Add files via upload

---
+++ src/Components/pages/Services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../App.css';
+
+
+const Services = () => {
+  return (
+    <div>
+      <h1 className='services'>Services</h1>
+    </div>
+  )
+}
+
+export default Services
+
+
+  
